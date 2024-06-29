8e1ba2f024dc4b3e4461c27971ca6a398a38a3fe
syorito-hatsuki <enderdron3@gmail.com>
2024-06-29 23:52:06 +0300

    Added gravity

--- src/main/kotlin/dev/syoritohatsuki/yetanotherairdrop/entity/projectile/AirDropEntity.kt
+++ src/main/kotlin/dev/syoritohatsuki/yetanotherairdrop/entity/projectile/AirDropEntity.kt
@@ -3,6 +3,7 @@ package dev.syoritohatsuki.yetanotherairdrop.entity.projectile
 import dev.syoritohatsuki.yetanotherairdrop.entity.EntityTypeRegistry
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.MovementType
 import net.minecraft.entity.data.DataTracker
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.world.World
@@ -14,11 +15,31 @@ class AirDropEntity(type: EntityType<AirDropEntity>, world: World) : Entity(type
         this.yaw = (random.nextDouble() * 360.0).toFloat()
         this.setVelocity(
             (random.nextDouble() * 0.2f - 0.1f) * 2.0,
-            random.nextDouble() * 0.2 * 2.0, (random.nextDouble() * 0.2f - 0.1f) * 2.0
+            random.nextDouble() * 0.2 * 2.0,
+            (random.nextDouble() * 0.2f - 0.1f) * 2.0
         )
     }
 
-    override fun getGravity(): Double = 5.0
+    override fun tick() {
+        super.tick()
+        prevX = x
+        prevY = y
+        prevZ = z
+        applyGravity()
+
+        move(MovementType.SELF, velocity)
+        var f = 0.98f
+        if (isOnGround) {
+            f = world.getBlockState(velocityAffectingPos).block.slipperiness * 0.98f
+        }
+
+        velocity = velocity.multiply(f.toDouble(), 0.98, f.toDouble())
+        if (isOnGround) {
+            velocity = velocity.multiply(1.0, -0.9, 1.0)
+        }
+    }
+
+    override fun getGravity(): Double = 0.0005
 
     override fun initDataTracker(builder: DataTracker.Builder) {
     }
