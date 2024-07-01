419d14ac96ca32a42bdc8f43dab0cc8ade7f7f05
syorito-hatsuki <enderdron3@gmail.com>
2024-07-02 01:27:27 +0300

    Removed unused code

--- src/main/kotlin/dev/syoritohatsuki/yetanotherairdrop/world/AirDropManager.kt
+++ src/main/kotlin/dev/syoritohatsuki/yetanotherairdrop/world/AirDropManager.kt
@@ -1,11 +1,8 @@
 package dev.syoritohatsuki.yetanotherairdrop.world
 
-import dev.syoritohatsuki.yetanotherairdrop.YetAnotherAirDrop.logger
 import dev.syoritohatsuki.yetanotherairdrop.entity.EntityTypeRegistry
 import net.minecraft.entity.SpawnReason
-import net.minecraft.entity.SpawnRestriction
-import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.registry.entry.RegistryEntry
+import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.MathHelper
@@ -14,15 +11,11 @@ import net.minecraft.world.GameRules
 import net.minecraft.world.Heightmap
 import net.minecraft.world.WorldView
 import net.minecraft.world.level.ServerWorldProperties
-import net.minecraft.world.poi.PointOfInterestStorage
-import net.minecraft.world.poi.PointOfInterestType
-import net.minecraft.world.poi.PointOfInterestTypes
 import net.minecraft.world.spawner.SpecialSpawner
 
-class AirDropManager(private val properties: ServerWorldProperties) : SpecialSpawner {
+class AirDropManager(properties: ServerWorldProperties) : SpecialSpawner {
 
     companion object {
-        private const val LOGGER_PREFIX = "AirDropManager\$"
         private val random: Random = Random.create()
         const val DEFAULT_SPAWN_TIMER: Int = 200
         const val DEFAULT_SPAWN_DELAY: Int = 24000
@@ -41,93 +34,63 @@ class AirDropManager(private val properties: ServerWorldProperties) : SpecialSpa
         spawnChance = properties.wanderingTraderSpawnChance
 
         if (spawnChance == 0) {
-            spawnChance = 25
+            spawnChance = DEFAULT_SPAWN_CHANCE
             properties.wanderingTraderSpawnChance = spawnChance
         }
         if (spawnDelay == 0) {
-            spawnDelay = 24000
+            spawnDelay = DEFAULT_SPAWN_DELAY
             properties.wanderingTraderSpawnDelay = spawnDelay
         }
     }
 
     override fun spawn(world: ServerWorld, spawnMonsters: Boolean, spawnAnimals: Boolean): Int {
-        logger.info("${LOGGER_PREFIX}spawn call")
-        if (--spawnTimer > 0) {
-            logger.info("${LOGGER_PREFIX}spawn if (--spawnTimer > 0 | ${spawnTimer})")
-            return 0
-        } else {
-            logger.info("${LOGGER_PREFIX}spawn else 1")
-            spawnTimer = DEFAULT_SPAWN_TIMER
-            spawnDelay -= DEFAULT_SPAWN_TIMER
-            properties.wanderingTraderSpawnDelay = spawnDelay
-//            if (spawnDelay > 0) {
-//                logger.info("${LOGGER_PREFIX}spawn if(spawnDelay > 0) | $spawnDelay")
-//                return 0
-//            } else {
-                logger.info("${LOGGER_PREFIX}spawn else 2")
+        when {
+            --spawnTimer > 0 -> return 0
+            else -> {
+                spawnTimer = DEFAULT_SPAWN_TIMER
+                spawnDelay -= DEFAULT_SPAWN_TIMER
                 spawnDelay = DEFAULT_SPAWN_DELAY
-                if (!world.gameRules.getBoolean(GameRules.DO_MOB_SPAWNING)) {
-                    return 0
-                } else {
-                    logger.info("${LOGGER_PREFIX}spawn else 3")
-                    val i = spawnChance
-                    spawnChance = MathHelper.clamp(spawnChance + DEFAULT_SPAWN_CHANCE, MIN_SPAWN_CHANCE, MAX_SPAWN_CHANCE)
-                    properties.wanderingTraderSpawnChance = spawnChance
-                    val ddd = random.nextInt(100)
-//                    if (ddd > i) {
-//                        logger.info("${LOGGER_PREFIX}spawn if () | $ddd")
-//                        return 0
-                    /*} else*/ if (trySpawn(world)) {
-                        logger.info("${LOGGER_PREFIX}spawn trySpawn")
-                        spawnChance = 25
-                        return 1
-                    } else {
-                        logger.info("${LOGGER_PREFIX}spawn else 4")
-                        return 0
+                return when {
+                    !world.gameRules.getBoolean(GameRules.DO_MOB_SPAWNING) -> 0
+                    else -> {
+                        spawnChance =
+                            MathHelper.clamp(spawnChance + DEFAULT_SPAWN_CHANCE, MIN_SPAWN_CHANCE, MAX_SPAWN_CHANCE)
+                        return when {
+                            trySpawn(world) -> {
+                                spawnChance = 25
+                                1
+                            }
+
+                            else -> 0
+                        }
                     }
-//                }
+                }
             }
         }
     }
 
     private fun trySpawn(world: ServerWorld): Boolean {
-        val playerEntity: PlayerEntity? = world.randomAlivePlayer
-        if (playerEntity == null) {
-            logger.info("${LOGGER_PREFIX}trySpawn player null :(")
-            return true
-//        } else if (random.nextInt(10) != 0) {
-//            logger.info("${LOGGER_PREFIX}trySpawn random miss")
-//            return false
-        } else {
-            logger.info("${LOGGER_PREFIX}trySpawn TRY")
-            val blockPos = playerEntity.blockPos
-            val optional = world.pointOfInterestStorage.getPosition({ poiType: RegistryEntry<PointOfInterestType> ->
-                poiType.matchesKey(PointOfInterestTypes.MEETING)
-            }, { _: BlockPos? -> true }, blockPos, 48, PointOfInterestStorage.OccupationStatus.ANY
-            )
-            val blockPos3: BlockPos? = getNearbySpawnPos(world, optional.orElse(blockPos) as BlockPos, 48)
-            if (blockPos3 != null && this.doesNotSuffocateAt(world, blockPos3)) {
-                logger.info("${LOGGER_PREFIX}trySpawn SPAWN")
-                val airDropEntity = EntityTypeRegistry.AIR_DROP.spawn(world, blockPos3, SpawnReason.EVENT)
-                if (airDropEntity != null) return true
-            }
-            logger.info("${LOGGER_PREFIX}trySpawn not suffocate")
-            return false
+        val playerEntity: ServerPlayerEntity = world.randomAlivePlayer ?: return true
+        val blockPos3: BlockPos? = getNearbySpawnPos(world, playerEntity.blockPos, 48)
+
+        if (blockPos3 != null && this.doesNotSuffocateAt(world, blockPos3)) {
+            EntityTypeRegistry.AIR_DROP.spawn(world, blockPos3.up(50), SpawnReason.EVENT) ?: return false
         }
+
+        return false
     }
 
     private fun getNearbySpawnPos(world: WorldView, pos: BlockPos, range: Int): BlockPos? {
         var blockPos: BlockPos? = null
-        val spawnLocation = SpawnRestriction.getLocation(EntityTypeRegistry.AIR_DROP)
 
-        for (i in 0..9) {
+        repeat(9) {
             val j = pos.x + random.nextInt(range * 2) - range
             val k = pos.z + random.nextInt(range * 2) - range
             val l = world.getTopY(Heightmap.Type.WORLD_SURFACE, j, k)
             val blockPos2 = BlockPos(j, l, k)
-            if (spawnLocation.isSpawnPositionOk(world, blockPos2, EntityTypeRegistry.AIR_DROP)) {
+            if (world.isAir(blockPos2)) {
                 blockPos = blockPos2
-                break
+                return@repeat
             }
         }
 
