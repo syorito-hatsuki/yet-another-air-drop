a544f5a331de5dc2698c1747f714cc7c703c18df
syorito-hatsuki <enderdron3@gmail.com>
2024-07-17 17:35:43 +0300

    Added new settings for datapack

--- src/main/kotlin/dev/syoritohatsuki/yetanotherairdrop/dto/Drop.kt
+++ src/main/kotlin/dev/syoritohatsuki/yetanotherairdrop/dto/Drop.kt
@@ -7,5 +7,8 @@ data class Drop(
     val name: String,
     val message: String? = null,
     val sound: String? = null,
+    // Under question. Maybe will be removed and replaced with auto search
+    val heightLimit: Int? = null,
+    val safePlatform: Boolean = false,
     val lootTables: Set<String> = emptySet()
 )
