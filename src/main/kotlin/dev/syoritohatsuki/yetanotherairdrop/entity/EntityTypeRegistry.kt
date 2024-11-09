617afc0d86270a66622d35adbb9271bd8d29a965
syorito-hatsuki <enderdron3@gmail.com>
2024-11-09 17:34:19 +0200

    Updated entity registry

--- src/main/kotlin/dev/syoritohatsuki/yetanotherairdrop/entity/EntityTypeRegistry.kt
+++ src/main/kotlin/dev/syoritohatsuki/yetanotherairdrop/entity/EntityTypeRegistry.kt
@@ -7,16 +7,20 @@ import net.minecraft.entity.EntityType
 import net.minecraft.entity.SpawnGroup
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.RegistryKeys
 import net.minecraft.util.Identifier
 
 
 object EntityTypeRegistry {
-    val AIR_DROP = register("air_drop", EntityType.Builder.create(::AirDropEntity, SpawnGroup.MISC)
-        .dimensions(1f, 1f)
-        .maxTrackingRange(32)
-        .trackingTickInterval(2))
-
-    private fun <T : Entity> register(id: String, type: EntityType.Builder<T>): EntityType<T> = Registry.register(
-        Registries.ENTITY_TYPE, Identifier.of(YetAnotherAirDrop.MOD_ID, id), type.build(id)
+    val AIR_DROP = register(
+        Identifier.of(YetAnotherAirDrop.MOD_ID, "air_drop"),
+        EntityType.Builder.create(::AirDropEntity, SpawnGroup.MISC).dimensions(1f, 1f).maxTrackingRange(32)
+            .trackingTickInterval(2)
     )
+
+    private fun <T : Entity> register(identifier: Identifier, type: EntityType.Builder<T>): EntityType<T> =
+        Registry.register(
+            Registries.ENTITY_TYPE, identifier, type.build(RegistryKey.of(RegistryKeys.ENTITY_TYPE, identifier))
+        )
 }
