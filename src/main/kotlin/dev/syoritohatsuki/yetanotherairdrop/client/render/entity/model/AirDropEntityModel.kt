f60a28b15c56fd4a19afe4a3e58434f4e7342467
syorito-hatsuki <enderdron3@gmail.com>
2025-03-12 10:59:26 +0200

    Updated texture and model

--- src/main/kotlin/dev/syoritohatsuki/yetanotherairdrop/client/render/entity/model/AirDropEntityModel.kt
+++ src/main/kotlin/dev/syoritohatsuki/yetanotherairdrop/client/render/entity/model/AirDropEntityModel.kt
@@ -1,91 +1,152 @@
-package dev.syoritohatsuki.yetanotherairdrop.client.render.entity.model
+// Made with Blockbench 4.12.3
+// Exported for Minecraft version 1.17+ for Yarn
+// Paste this class into your mod and generate all required imports
+
+package dev.syoritohatsuki.yetanotherairdrop.client.render.entity.model;
 
 import net.minecraft.client.model.*
 import net.minecraft.client.render.entity.model.EntityModel
 import net.minecraft.client.render.entity.state.EntityRenderState
 
-class AirDropEntityModel(root: ModelPart) : EntityModel<EntityRenderState?>(root) {
+class AirDropEntityModel(root: ModelPart) : EntityModel<EntityRenderState>(root) {
     companion object {
-        fun getTexturedModelData(): TexturedModelData = TexturedModelData.of(ModelData().apply {
-            root.addChild(
+        fun getTexturedModelData(): TexturedModelData {
+            val modelData = ModelData()
+            val modelPartData = modelData.root
+
+            val root = modelPartData.addChild(
                 "root",
-                ModelPartBuilder.create().uv(0, 0).cuboid(-8.0f, 0.0f, -8.0f, 16.0f, 16.0f, 16.0f, Dilation(0.0f)),
-                ModelTransform.pivot(0.0f, 0.0f, 0.0f)
-            ).let { root ->
-                root.addChild(
-                    "balloons", ModelPartBuilder.create(), ModelTransform.pivot(0.0f, 16.0f, 0.0f)
-                ).let { balloons ->
-                    balloons.addChild(
-                        "balloon_1",
-                        ModelPartBuilder.create().uv(0, 77)
-                            .cuboid(-3.0f, 3.0f, -3.0f, 6.0f, 10.0f, 6.0f, Dilation(0.0f)).uv(88, 43)
-                            .cuboid(-1.0f, 2.0f, -1.0f, 2.0f, 1.0f, 2.0f, Dilation(0.0f)).uv(64, 0)
-                            .cuboid(-4.0f, 5.0f, -4.0f, 8.0f, 7.0f, 8.0f, Dilation(0.0f)).uv(88, 58)
-                            .cuboid(-0.5f, 0.0f, -0.5f, 1.0f, 2.0f, 1.0f, Dilation(0.0f)),
-                        ModelTransform.pivot(1.0f, 0.0f, -1.0f)
-                    )
+                ModelPartBuilder.create().uv(0, 0).cuboid(-8.0F, 0.0F, -8.0F, 16.0F, 16.0F, 16.0F, Dilation(0.0F)),
+                ModelTransform.pivot(0.0F, 0.0F, 0.0F)
+            )
 
-                    balloons.addChild(
-                        "balloon_2",
-                        ModelPartBuilder.create().uv(0, 77)
-                            .cuboid(-3.0f, 8.0f, -3.0f, 6.0f, 10.0f, 6.0f, Dilation(0.0f)).uv(88, 43)
-                            .cuboid(-1.0f, 7.0f, -1.0f, 2.0f, 1.0f, 2.0f, Dilation(0.0f)).uv(64, 0)
-                            .cuboid(-4.0f, 10.0f, -4.0f, 8.0f, 7.0f, 8.0f, Dilation(0.0f)).uv(88, 29)
-                            .cuboid(-0.5f, 0.0f, -0.5f, 1.0f, 7.0f, 1.0f, Dilation(0.0f)),
-                        ModelTransform.pivot(-7.5f, 0.0f, -2.5f)
-                    )
+            val balls = root.addChild(
+                "balls",
+                ModelPartBuilder.create().uv(0, 33).cuboid(-11.5F, 16.0F, 2.5F, 1.0F, 2.0F, 1.0F, Dilation(0.0F)),
+                ModelTransform.pivot(7.5F, 0.0F, -8.5F)
+            )
 
-                    balloons.addChild(
-                        "balloon_3",
-                        ModelPartBuilder.create().uv(0, 77).cuboid(
-                            -3.0f, 14.375f, -3.0f, 6.0f, 10.0f, 6.0f, Dilation(0.0f)
-                        ).uv(88, 43).cuboid(-1.0f, 13.375f, -1.0f, 2.0f, 1.0f, 2.0f, Dilation(0.0f)).uv(64, 0)
-                            .cuboid(-4.0f, 16.375f, -4.0f, 8.0f, 7.0f, 8.0f, Dilation(0.0f)).uv(88, 15)
-                            .cuboid(-0.5f, 0.375f, -0.5f, 1.0f, 13.0f, 1.0f, Dilation(0.0f)),
-                        ModelTransform.pivot(-6.0f, -0.375f, 6.0f)
-                    )
+            balls.addChild(
+                "ball",
+                ModelPartBuilder.create().uv(38, 33).cuboid(-14.0F, 19.0F, 0.0F, 6.0F, 10.0F, 6.0F, Dilation(0.0F))
+                    .uv(5, 33).cuboid(-15.0F, 21.0F, -1.0F, 8.0F, 7.0F, 8.0F, Dilation(0.0F)).uv(5, 49)
+                    .cuboid(-12.0F, 18.0F, 2.0F, 2.0F, 1.0F, 2.0F, Dilation(0.0F)),
+                ModelTransform.pivot(0.0F, 0.0F, 0.0F)
+            )
 
-                    balloons.addChild(
-                        "balloon_4",
-                        ModelPartBuilder.create().uv(0, 77).cuboid(
-                            -3.0f, 36.0f, -3.0f, 6.0f, 10.0f, 6.0f, Dilation(0.0f)
-                        ).uv(88, 43).cuboid(-1.0f, 35.0f, -1.0f, 2.0f, 1.0f, 2.0f, Dilation(0.0f)).uv(64, 0)
-                            .cuboid(-4.0f, 38.0f, -4.0f, 8.0f, 7.0f, 8.0f, Dilation(0.0f)).uv(72, 79)
-                            .cuboid(-0.5f, 0.0f, -0.5f, 1.0f, 35.0f, 1.0f, Dilation(0.0f)),
-                        ModelTransform.pivot(-1.5f, 0.0f, 3.5f)
-                    )
+            val balls2 = root.addChild(
+                "balls2",
+                ModelPartBuilder.create().uv(0, 33).cuboid(-11.5F, 1.0F, 2.5F, 1.0F, 17.0F, 1.0F, Dilation(0.0F)),
+                ModelTransform.pivot(12.5F, 15.0F, -10.0F)
+            )
 
-                    balloons.addChild(
-                        "balloon_5",
-                        ModelPartBuilder.create().uv(0, 77).cuboid(
-                            -3.0f, 25.0f, -3.0f, 6.0f, 10.0f, 6.0f, Dilation(0.0f)
-                        ).uv(88, 43).cuboid(-1.0f, 24.0f, -1.0f, 2.0f, 1.0f, 2.0f, Dilation(0.0f)).uv(64, 0)
-                            .cuboid(-4.0f, 27.0f, -4.0f, 8.0f, 7.0f, 8.0f, Dilation(0.0f)).uv(76, 79)
-                            .cuboid(-0.5f, 0.0f, -0.5f, 1.0f, 24.0f, 1.0f, Dilation(0.0f)),
-                        ModelTransform.pivot(4.0f, 0.0f, 5.0f)
-                    )
+            balls2.addChild(
+                "ball2",
+                ModelPartBuilder.create().uv(5, 49).cuboid(-7.0F, 33.0F, 0.5F, 2.0F, 1.0F, 2.0F, Dilation(0.0F))
+                    .uv(5, 33).cuboid(-10.0F, 36.0F, -2.5F, 8.0F, 7.0F, 8.0F, Dilation(0.0F)).uv(38, 33)
+                    .cuboid(-9.0F, 34.0F, -1.5F, 6.0F, 10.0F, 6.0F, Dilation(0.0F)),
+                ModelTransform.pivot(-5.0F, -15.0F, 1.5F)
+            )
 
-                    balloons.addChild(
-                        "balloon_6",
-                        ModelPartBuilder.create().uv(0, 77).cuboid(
-                            -3.0f, 15.0f, -3.0f, 6.0f, 10.0f, 6.0f, Dilation(0.0f)
-                        ).uv(88, 43).cuboid(-1.0f, 14.0f, -1.0f, 2.0f, 1.0f, 2.0f, Dilation(0.0f)).uv(64, 0)
-                            .cuboid(-4.0f, 17.0f, -4.0f, 8.0f, 7.0f, 8.0f, Dilation(0.0f)).uv(84, 79)
-                            .cuboid(-0.5f, 0.0f, -0.5f, 1.0f, 14.0f, 1.0f, Dilation(0.0f)),
-                        ModelTransform.pivot(7.0f, 0.0f, -3.0f)
-                    )
+            val balls3 = root.addChild(
+                "balls3",
+                ModelPartBuilder.create().uv(0, 33).cuboid(-11.5F, 12.0F, 2.5F, 1.0F, 6.0F, 1.0F, Dilation(0.0F)),
+                ModelTransform.pivot(18.5F, 4.0F, -8.5F)
+            )
 
-                    balloons.addChild(
-                        "balloon_7",
-                        ModelPartBuilder.create().uv(0, 77).cuboid(
-                            -3.0f, 17.0f, -3.0f, 6.0f, 10.0f, 6.0f, Dilation(0.0f)
-                        ).uv(88, 43).cuboid(-1.0f, 16.0f, -1.0f, 2.0f, 1.0f, 2.0f, Dilation(0.0f)).uv(64, 0)
-                            .cuboid(-4.0f, 19.0f, -4.0f, 8.0f, 7.0f, 8.0f, Dilation(0.0f)).uv(80, 79)
-                            .cuboid(-0.5f, 0.0f, -0.5f, 1.0f, 16.0f, 1.0f, Dilation(0.0f)),
-                        ModelTransform.pivot(-1.0f, 0.0f, -7.0f)
-                    )
-                }
-            }
-        }, 128, 128)
+            balls3.addChild(
+                "ball3",
+                ModelPartBuilder.create().uv(38, 33).cuboid(-3.0F, 23.0F, 0.0F, 6.0F, 10.0F, 6.0F, Dilation(0.0F))
+                    .uv(5, 33).cuboid(-4.0F, 25.0F, -1.0F, 8.0F, 7.0F, 8.0F, Dilation(0.0F)).uv(5, 49)
+                    .cuboid(-1.0F, 22.0F, 2.0F, 2.0F, 1.0F, 2.0F, Dilation(0.0F)),
+                ModelTransform.pivot(-11.0F, -4.0F, 0.0F)
+            )
+
+            val balls4 = root.addChild(
+                "balls4",
+                ModelPartBuilder.create().uv(0, 33).cuboid(-11.5F, -12.0F, 2.5F, 1.0F, 30.0F, 1.0F, Dilation(0.0F)),
+                ModelTransform.pivot(12.0F, 28.0F, -5.0F)
+            )
+
+            balls4.addChild(
+                "ball4",
+                ModelPartBuilder.create().uv(38, 33).cuboid(-9.5F, 47.0F, 3.5F, 6.0F, 10.0F, 6.0F, Dilation(0.0F))
+                    .uv(5, 33).cuboid(-10.5F, 49.0F, 2.5F, 8.0F, 7.0F, 8.0F, Dilation(0.0F)).uv(5, 49)
+                    .cuboid(-7.5F, 46.0F, 5.5F, 2.0F, 1.0F, 2.0F, Dilation(0.0F)),
+                ModelTransform.pivot(-4.5F, -28.0F, -3.5F)
+            )
+
+            val balls5 = root.addChild(
+                "balls5",
+                ModelPartBuilder.create().uv(0, 33).cuboid(-11.5F, 3.0F, 2.5F, 1.0F, 15.0F, 1.0F, Dilation(0.0F)),
+                ModelTransform.pivot(18.0F, 13.0F, -1.0F)
+            )
+
+            balls5.addChild(
+                "ball5",
+                ModelPartBuilder.create().uv(38, 33).cuboid(-3.5F, 32.0F, 7.5F, 6.0F, 10.0F, 6.0F, Dilation(0.0F))
+                    .uv(5, 33).cuboid(-4.5F, 34.0F, 6.5F, 8.0F, 7.0F, 8.0F, Dilation(0.0F)).uv(5, 49)
+                    .cuboid(-1.5F, 31.0F, 9.5F, 2.0F, 1.0F, 2.0F, Dilation(0.0F)),
+                ModelTransform.pivot(-10.5F, -13.0F, -7.5F)
+            )
+
+            val balls6 = root.addChild(
+                "balls6",
+                ModelPartBuilder.create().uv(0, 33).cuboid(-11.5F, 15.0F, 2.5F, 1.0F, 3.0F, 1.0F, Dilation(0.0F)),
+                ModelTransform.pivot(15.5F, 1.0F, 4.5F)
+            )
+
+            balls6.addChild(
+                "ball6",
+                ModelPartBuilder.create().uv(38, 33).cuboid(-6.0F, 20.0F, 13.0F, 6.0F, 10.0F, 6.0F, Dilation(0.0F))
+                    .uv(5, 33).cuboid(-7.0F, 22.0F, 12.0F, 8.0F, 7.0F, 8.0F, Dilation(0.0F)).uv(5, 49)
+                    .cuboid(-4.0F, 19.0F, 15.0F, 2.0F, 1.0F, 2.0F, Dilation(0.0F)),
+                ModelTransform.pivot(-8.0F, -1.0F, -13.0F)
+            )
+
+            val balls7 = root.addChild(
+                "balls7",
+                ModelPartBuilder.create().uv(0, 33).cuboid(-11.5F, -5.0F, 2.5F, 1.0F, 23.0F, 1.0F, Dilation(0.0F)),
+                ModelTransform.pivot(10.0F, 21.0F, 2.25F)
+            )
+
+            balls7.addChild(
+                "ball7",
+                ModelPartBuilder.create().uv(38, 33).cuboid(-11.5F, 40.0F, 10.75F, 6.0F, 10.0F, 6.0F, Dilation(0.0F))
+                    .uv(5, 33).cuboid(-12.5F, 42.0F, 9.75F, 8.0F, 7.0F, 8.0F, Dilation(0.0F)).uv(5, 49)
+                    .cuboid(-9.5F, 39.0F, 12.75F, 2.0F, 1.0F, 2.0F, Dilation(0.0F)),
+                ModelTransform.pivot(-2.5F, -21.0F, -10.75F)
+            )
+
+            val balls8 = root.addChild(
+                "balls8",
+                ModelPartBuilder.create().uv(0, 33).cuboid(-11.5F, 14.0F, 2.5F, 1.0F, 4.0F, 1.0F, Dilation(0.0F)),
+                ModelTransform.pivot(5.0F, 2.0F, 3.5F)
+            )
+
+            balls8.addChild(
+                "ball8",
+                ModelPartBuilder.create().uv(38, 33).cuboid(-16.5F, 21.0F, 12.0F, 6.0F, 10.0F, 6.0F, Dilation(0.0F))
+                    .uv(5, 33).cuboid(-17.5F, 23.0F, 11.0F, 8.0F, 7.0F, 8.0F, Dilation(0.0F)).uv(5, 49)
+                    .cuboid(-14.5F, 20.0F, 14.0F, 2.0F, 1.0F, 2.0F, Dilation(0.0F)),
+                ModelTransform.pivot(2.5F, -2.0F, -12.0F)
+            )
+
+            val balls9 = root.addChild(
+                "balls9",
+                ModelPartBuilder.create().uv(0, 33).cuboid(-11.5F, 4.0F, 2.5F, 1.0F, 14.0F, 1.0F, Dilation(0.0F)),
+                ModelTransform.pivot(3.5F, 12.0F, -3.5F)
+            )
+
+			balls9.addChild(
+                "ball9",
+                ModelPartBuilder.create().uv(38, 33).cuboid(-18.0F, 31.0F, 5.0F, 6.0F, 10.0F, 6.0F, Dilation(0.0F))
+                    .uv(5, 33).cuboid(-19.0F, 33.0F, 4.0F, 8.0F, 7.0F, 8.0F, Dilation(0.0F)).uv(5, 49)
+                    .cuboid(-16.0F, 30.0F, 7.0F, 2.0F, 1.0F, 2.0F, Dilation(0.0F)),
+                ModelTransform.pivot(4.0F, -12.0F, -5.0F)
+            )
+
+            return TexturedModelData.of(modelData, 64, 64);
+        }
     }
 }
