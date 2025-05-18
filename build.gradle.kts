acf9816e7de566ec09790c0f06d493a22d45d3be
syorito-hatsuki <enderdron3@gmail.com>
2025-05-18 21:22:44 +0300

    Here we go again

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     id("fabric-loom")
     kotlin("jvm")
     kotlin("plugin.serialization")
+    id("com.modrinth.minotaur")
 }
 
 base {
@@ -50,7 +51,34 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 }
 
+modrinth {
+    token.set(System.getenv("MODRINTH_TOKEN"))
+    projectId.set("yet-another-air-drop")
+    versionName.set("Yet Another Air Drop $modVersion")
+    versionNumber.set(modVersion)
+    versionType.set("release")
+    uploadFile.set(tasks.remapJar)
+    gameVersions.addAll("1.21.4")
+    loaders.add("fabric")
+    changelog.set(rootProject.file("CHANGELOG.md").readText())
+    dependencies {
+        required.project("fabric-api", "fabric-language-kotlin")
+        embedded.project("ducky-updater-lib", "modmenu-badges-lib")
+    }
+}
+
 tasks {
+    named("modrinth").configure {
+        doLast {
+            val uploadTask = this@configure as com.modrinth.minotaur.TaskModrinthUpload
+            @Suppress("UnstableApiUsage")
+            val uploadInfo = uploadTask.uploadInfo ?: return@doLast
+            val url = "https://modrinth.com/mod/yet-another-air-drop/version/${uploadInfo.id}"
+            println(url)
+            rootProject.file("build/modrinth_url.txt").writeText(url)
+        }
+    }
+
     withType<JavaCompile> {
         options.encoding = "UTF-8"
         sourceCompatibility = javaVersion.toString()
